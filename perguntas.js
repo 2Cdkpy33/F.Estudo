85ad79fd1809e9e1de136642eacb84dd9f0f5c85
2Cdkpy33 <0000111668312xsp@al.educacao.sp.gov.br>
2024-12-02 07:57:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'blocos',
+    'qual é o bloco inquebravel?',
+    'bedrock'
+)
+
+criaCartao(
+    'mobs',
+    'qual mob teletransporta?',
+    'enderman'
+)
+
+criaCartao(
+    'itens',
+    'qual arma é dropada de um afogado?',
+    'tridente'
+)
+
+criaCartao(
+    'comidas',
+    'qual é considerada a melhor comida?',
+    'cenoura dourada'
+)
